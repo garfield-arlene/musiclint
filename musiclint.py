bf126c672c02d9c651cbf3f4517f32fcd376c039
garfield <garfield@docboy.home.mygarfield.us>
2019-05-01 19:02:20 -0700

    Now reading tags

--- musiclint.py
+++ musiclint.py
@@ -11,7 +11,7 @@ parser = argparse.ArgumentParser()
 
 def readable_dir(prospective_dir):
     if not os.path.isdir(prospective_dir):
-        raise Exception("reafindmp3dable_dir:{0} is not a valid path".format(prospective_dir))
+        raise Exception("readable_dir:{0} is not a valid path".format(prospective_dir))
     if os.access(prospective_dir, os.R_OK):
         return prospective_dir
     else:
@@ -20,20 +20,15 @@ def readable_dir(prospective_dir):
 parser.add_argument(
     '-l', '--library',
     type=readable_dir,
+    # action="store_true",
     help="Specify the music library root directory",
-)
-
-parser.add_argument(
-    '-d', '--debug',
-    help="Print lots of debugging statements",
-    action="store_const", dest="loglevel", const=logging.DEBUG,
-    default=logging.WARNING,
 )
 
 parser.add_argument(
     '-v', '--verbosity',
     help="Increase verbosity",
     action="count",
+    default=0,
 )
 
 parser.add_argument(
@@ -53,13 +48,11 @@ if args.mp3:
     import plugins.processMp3
 
 def main():
-    print("hello")
-    
-    if args.verbosity >= 1:
+    if args.verbosity:
         logging.info("Library directory: " + args.library)
     
     if args.mp3:
-        if args.verbosity >= 1:
+        if args.verbosity:
             logging.info("Processing mp3 files")
         
         plugins.processMp3.processMP3Files(args.library, args.verbosity)
--- plugins/processMp3.py
+++ plugins/processMp3.py
@@ -2,6 +2,9 @@
 import logging
 import os
 import glob
+from mp3_tagger import MP3File, VERSION_1, VERSION_2, VERSION_BOTH
+from pprint import pprint
+from re import escape
 
 def processMP3Files(libPath, verbosity):
     if verbosity >= 2:
@@ -12,7 +15,13 @@ def processMP3Files(libPath, verbosity):
             if '.mp3' in filename:
                 if verbosity >= 2:
                     logging.info(os.path.join(root,filename))
+                
                 # read mp3 tags
+                print(filename)
+                mp3 = MP3File(os.path.join(root,filename))
+                tags = mp3.get_tags()
+                pprint(tags)
+
                 # search online music DB & pull tags
                 # compaire DB tags & file tags
 
